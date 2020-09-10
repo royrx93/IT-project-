9a45580b5c4fb9e2e33a8bd74475c2e75c4e74a4
iggy-1217 <62122098+iggy-1217@users.noreply.github.com>
2020-09-10 21:41:20 +0800

    simple login Page

--- Controller/loginController.js
+++ Controller/loginController.js
@@ -1,9 +1,50 @@
 const db = require('../model/index');
 
-const getloginPage = (req,res) => {
+const getLoginPage = (req,res) => {
     res.render('loginPage',{})
 };
 
+const loginToSystem = (req,res) =>{
+    let info = req.body;
+    let sql = 'select * from user where username=? and password=?';
+    let data = [
+        info.username,
+        info.password
+    ]
+    db.base(sql,data, (result)=>{
+        if (result.length ==1){
+            res.send("login Successful")
+        }else{
+            console.log(result)
+        }
+    })
+
+}
+
+const getUserInfo = (req,res) => {
+    let info = req.body;
+    let user = {};
+    for (let key in info ){
+        user[key] = info[key];
+    }
+
+    let sql = 'insert into user set ?'
+    db.base(sql, user, (result) =>{
+        if(result.affectedRows == 1){
+            res.redirect('/login')
+        }
+    })
+};
+
+
+const getRegisterPage = (req,res) => {
+    res.render('registerPage',{})
+};
+
+
 module.exports = {
-    getloginPage
+    getLoginPage,
+    getRegisterPage,
+    getUserInfo,
+    loginToSystem
 };
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,6 +2,9 @@ const express = require('express');
 const loginRouter = express.Router();
 const loginController = require('../Controller/loginController');
 
-loginRouter.get('/',(req,res) => loginController.getloginPage(req,res));
+loginRouter.get('/',(req,res) => loginController.getLoginPage(req,res));
+loginRouter.post('/',(req,res) => loginController.loginToSystem(req,res));
+loginRouter.get('/register',(req,res) => loginController.getRegisterPage(req,res));
+loginRouter.post('/register',(req,res) => loginController.getUserInfo(req,res));
 
 module.exports = loginRouter;
